aad141dc91c0afc69f610460bccdcd166a0bb256
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-07-28 11:41:14 +0530

    GH

--- rename-build.js
+++ rename-build.js
@@ -4,6 +4,27 @@ const path = require('path');
 const buildPath = path.join(__dirname, 'build');
 const distPath = path.join(__dirname, 'dist');
 
+// Function to delete a directory recursively
+const deleteFolderRecursive = function (folderPath) {
+    if (fs.existsSync(folderPath)) {
+        fs.readdirSync(folderPath).forEach((file) => {
+            const curPath = path.join(folderPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(folderPath);
+    }
+};
+
+// Delete the existing dist directory if it exists
+if (fs.existsSync(distPath)) {
+    deleteFolderRecursive(distPath);
+}
+
+// Rename the build directory to dist
 if (fs.existsSync(buildPath)) {
     fs.renameSync(buildPath, distPath);
     console.log('Build directory renamed to dist');
